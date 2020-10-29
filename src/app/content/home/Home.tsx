8288ba082b09c7998a0a0e2b555d920b7a1c3a44
Bartlomiej Perekitko <b.perekitko@gmail.com>
2020-10-29 08:44:27 +0100

    Change styles, add home content

--- src/app/content/home/Home.tsx
+++ src/app/content/home/Home.tsx
@@ -2,27 +2,32 @@ import { FunctionComponent } from 'react';
 import React from 'react';
 import styles from './home.module.scss';
 import { useWeb3Context } from '../../web3-context/web3-context';
-import Web3Model from '../../web3-context/web3-context.model';
 
 const Home: FunctionComponent<unknown> = () => {
-  const web3Context = useWeb3Context();
-  const content = prepareContent(web3Context);
+  const content = prepareContent();
 
   return (
     <div className={styles.home}>
-      <div>Welcome to the Ethernaut Solver!</div>
+      <div className={styles.title}>Welcome to the Ethernaut Solver!</div>
       {content}
     </div>
   );
 };
 
-const prepareContent = (web3Context: Web3Model): JSX.Element => {
-  const { address, balance, initialized } = web3Context;
+const prepareContent = (): JSX.Element => {
+  const { initialized } = useWeb3Context();
 
   return initialized ? (
     <>
-      <div>Your eth balance is: {balance}</div>
-      <div>Your eth address is: {address}</div>
+      <div className={styles.content}>
+        This app contains solutions to all levels of Ethernaut wargame created by OpenZeppelin. You can check it out{' '}
+        <a href='https://solidity-05.ethernaut.openzeppelin.com/' target='_blank' rel='noopener noreferrer'>
+          here
+        </a>
+        . You can try to solve the levels on your own and come back here to check your answers. Or you can solve them
+        right here. Make sure you are connected to Rinkeby test network, because it is the network used by Ethernaut.
+        Choose a level from the menu on the left and let&apos;s jump in!
+      </div>
     </>
   ) : (
     <div>Connect your wallet to continue</div>
